06b892cf4c2404c46321c2af916311d2149e9fb6
Cmarsman <chris_marsden@yahoo.com>
2024-06-05 17:41:03 -0500

    intro done

--- app.js
+++ app.js
@@ -3,8 +3,30 @@ console.log(
   "Follow the steps in the README.md file to complete the exercises:\n==========\n"
 );
 
-// Exercise 1
+const firstName = "Ted";
+let lastName = "Bundy";
+var age = 50;
 
-// Exercise 2
+lastName = "Lasso";
+age = 51;
 
-// Exercise 3
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensative = true;
+
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookMark = 456;
+let hasRead = true;
+
+console.log(`${title} by ${author} is ${pageCount} pages long.`);
+
+
